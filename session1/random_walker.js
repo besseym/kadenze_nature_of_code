60787271fcbb1382bb05f720cb75566f05bb86ec
Michael Bessey <besseym@gmail.com>
2019-05-14 08:48:14 -0400

    Walker with random accelleration

--- session1/random_walker.js
+++ session1/random_walker.js
@@ -13,17 +13,17 @@ function draw() {
 
 function Walker(){
 
-	this.pos = createVector(width/2, 0);
+	this.pos = createVector(width/2, height/2);
 	this.vel = createVector(0, 0);
-	this.acc = createVector(0, 0.1);
+
+	this.update = function(){
+		this.acc = createVector(random(-1, 1), random(-1, 1));
+		this.vel.add(this.acc);
+		this.pos.add(this.vel);
+	}
 
 	this.display = function(){
 		fill(255);
 		ellipse(this.pos.x, this.pos.y, 48, 48);
-	}
-
-	this.update = function(){
-		this.vel.add(this.acc);
-		this.pos.add(this.vel);
 	}
 }
