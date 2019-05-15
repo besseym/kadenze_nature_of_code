b4b8db909e039d70fd706c4e97e6653f693230df
Michael Bessey <besseym@gmail.com>
2019-05-14 23:44:35 -0400

    session 1 wrap up

--- session1/sketch.js
+++ session1/sketch.js
@@ -1,7 +1,48 @@
+var w, 
+	off = 0
+;
+
 function setup() {
-  // put setup code here
+	createCanvas(640, 360);
+	w = new Walker();
 }
 
 function draw() {
-  // put drawing code here
+	background(51);
+	w.display();
+	w.update();
+}
+
+function Walker(){
+
+	this.pos = createVector(width/2, height/2);
+	this.vel = createVector(0, 0);
+
+	this.acc = p5.Vector.fromAngle(random(TWO_PI));
+	this.acc.setMag(0.05);
+
+	this.update = function(){
+
+		// var mouse = createVector(mouseX, mouseY);
+		// this.acc = p5.Vector.sub(mouse, this.pos);
+		// this.acc.normalize();
+		// this.acc.mult(0.05);
+
+		// this.acc.rotate(0.08);
+
+		// this.vel.add(this.acc);
+		var x = noise(off) * width,
+			y = noise(off) * (height / 2)
+		;
+
+		off += 0.01;
+
+		this.pos = createVector(x, y);
+		// this.pos.add(this.vel);
+	}
+
+	this.display = function(){
+		fill(255);
+		ellipse(this.pos.x, this.pos.y, 48, 48);
+	}
 }
